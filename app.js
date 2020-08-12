8abbf824eca9d72ccb140927c719baddb8435af4
Phillip Musiime <phillipmusiime@gmail.com>
2020-08-12 11:47:03 +0300

    localstorage, state and refactoring

--- app.js
+++ app.js
@@ -1,86 +1,106 @@
-let idx = 0;
-
-
-
-
+let len = JSON.parse(localStorage.getItem('books')).length;
+let idx;
+if (len === 0) {
+  idx = 0;
+} else {
+  idx = JSON.parse(localStorage.getItem('books'))[len - 1].id + 1;
+}
 
 function Book(title, author, pages, read = false) {
-	this.id = idx++;
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
+  this.id = idx++;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 const container = document.querySelector('.container');
-
-
-
-let local = JSON.parse(localStorage['books']);
-function render() {
-	const displayBooks = local.map(function (item) {
-		return `<div class="book-item" id=${item.id}>
-				Author: ${item.author} => 
-				Title: ${item.title} => 
-				${item.pages} pages
-				<input type="checkbox" id="status" name="status">
-				<label for="status">Read &nbsp; &nbsp;</label>
-				<button onclick="removeElement(${item.id})">Remove</button>
-				</div>`;
-	});
-	container.innerHTML = displayBooks.join('');
-}
-
 const addBook = document.getElementById('addBook');
 const form = document.querySelector('.hidden');
+const add = document.getElementById('add');
+
+
+function render() {
+  const local = JSON.parse(localStorage.getItem('books'));
+  const displayBooks = local.map(function (item) {
+    if (item.read === true) {
+      checked = 'checked';
+    } else {
+      checked = "";
+    }
+    return `<div class="book-item" id=${item.id}>
+    Author: ${item.author} => 
+    Title: ${item.title} => 
+    ${item.pages} pages
+    <input type="checkbox" id="status" name="status" class="status" ${checked}>
+    <label for="status">Read &nbsp; &nbsp;</label>
+    <button onclick="removeFromLocalStorageArray('books',${item.id})">Remove</button>
+    </div>`;
+
+  });
+  container.innerHTML = displayBooks.join('');
+};
 
 
 addBook.addEventListener('click', () => {
-	form.classList.toggle('hidden');
+  form.classList.toggle('hidden');
 });
 
-const add = document.getElementById('add');
 
-add.addEventListener('click', (e) => {
-	e.preventDefault();
-	const author = document.getElementById('author').value;
-	const title = document.getElementById('title').value;
-	const pages = document.getElementById('pages').value;
-	let newBook = new Book(title, author, pages);
-	addToLocalStorageArray('books', newBook);
+function clear() {
+  document.getElementById('author').value = "";
+  document.getElementById('title').value = "";
+  document.getElementById('pages').value = "";
+}
 
-	render();
+
+add.addEventListener('click', () => {
+  const authorValue = document.getElementById('author').value;
+  const titleValue = document.getElementById('title').value;
+  const pagesValue = document.getElementById('pages').value;
+  let newBook = new Book(titleValue, authorValue, pagesValue);
+  addToLocalStorageArray('books', newBook);
+  clear();
 });
 
 render();
 
-function removeElement(id) {
-	books = books.filter((items) => items.id != id);
-	var element = document.getElementById(id);
-	element.parentNode.removeChild(element);
-}
+const checkboxes = document.querySelectorAll('#status');
 
-Book.prototype.updateStatus = (id) => {
-	books[id].read == false ? (books[id].read = true) : (books[id].read = false);
-};
+checkboxes.forEach((box) => {
+  box.addEventListener('click', (e) => {
+    let idx = e.currentTarget.parentNode.id;
+    updateLocalStorageArray('books', idx);
 
-const items = document.querySelectorAll('#status');
-items.forEach((box) => {
-	box.addEventListener('click', (e) => {
-		let idx = e.currentTarget.parentNode.id;
-		books[idx].updateStatus(idx);
-		console.log(books[idx]);
-	});
+  });
 });
 
-// function updateStorage() {
-// 	localStorage.setItem('books', JSON.stringify(books));
-// }
-
 function addToLocalStorageArray(name, value) {
-	var existing = JSON.parse(localStorage.getItem(name));
-	console.log(existing);
-	existing.push(value);
-	// localStorage.setItem(name, existing.toString());
-	localStorage.setItem(name, JSON.stringify(value));
+  let existing = JSON.parse(localStorage.getItem(name));
+  existing.push(value);
+  localStorage.setItem(name, JSON.stringify(existing));
+  render();
 }
+
+
+function removeFromLocalStorageArray(name, id) {
+  let existing = JSON.parse(localStorage.getItem(name));
+  existing = existing.filter((items) => items.id !== id);
+  localStorage.setItem(name, JSON.stringify(existing));
+  location.reload();
+}
+
+function updateLocalStorageArray(name, id) {
+  let existing = JSON.parse(localStorage.getItem(name));
+  let item = existing.forEach((i) => {
+    if (i.id == id) {
+      if (i.read === false) {
+        i.read = true;
+      } else {
+        i.read = false;
+      }
+    }
+  })
+  localStorage.setItem(name, JSON.stringify(existing));
+}
+
