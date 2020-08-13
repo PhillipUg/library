0ae62f5d2a5763b4cecf5e2b8980510c6ce95f6d
Phillip Musiime <phillipmusiime@gmail.com>
2020-08-13 11:22:07 +0300

    added module

---
+++ content.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+const container = document.querySelector('.container');
+const addBook = document.getElementById('addBook');
+const form = document.querySelector('.hidden');
+const add = document.getElementById('add');
+
+const authorValue = document.getElementById('author').value;
+const titleValue = document.getElementById('title').value;
+const pagesValue = document.getElementById('pages').value;
+
+const checkboxes = document.querySelectorAll('#status');
+
+export { container, addBook, add, form, authorValue, titleValue, pagesValue, checkboxes };
